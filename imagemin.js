6a7f33c5f722613974f968bff6212b0d01ed0be6
mlloewen <mlloewen@gmail.com>
2015-05-28 22:53:17 -0600

    imagemin

--- imagemin.js
+++ imagemin.js
@@ -2,10 +2,7 @@ var Imagemin = require('imagemin');
 var imageminMozjpeg = require('imagemin-mozjpeg');
 
 new Imagemin()
-    .src('images/*.{gif,jpg,png,svg}')
-    .dest('build/images')
-    .use(imageminMozjpeg({quality: 75}));
-    .run(function (err, files) {
-        console.log(files[0]);
-        // => {path: 'build/images/foo.jpg', contents: <Buffer 89 50 4e ...>}
-    });
+    .src('images/*.{gif,jpg,png,svg,JPG}')
+    .dest('dist/images')
+    .use(imageminMozjpeg({quality: 75}))
+    .run();
