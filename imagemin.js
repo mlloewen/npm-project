ab2aa08d735e729c9d8c9c6eba60c952eac83ac1
mlloewen <mlloewen@gmail.com>
2015-06-01 22:22:28 -0600

    tests

--- imagemin.js
+++ imagemin.js
@@ -1,8 +1,10 @@
 var Imagemin = require('imagemin');
 var imageminMozjpeg = require('imagemin-mozjpeg');
+var imageminOptipng = require('imagemin-optipng');
 
 new Imagemin()
     .src('images/*.{gif,jpg,png,svg,JPG}')
     .dest('dist/images')
     .use(imageminMozjpeg({quality: 75}))
+    .use(imageminOptipng({optimizationLevel: 7}))
     .run();
